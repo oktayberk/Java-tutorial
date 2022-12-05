45378d4a4a6626d5aa797df9da184769285b9821
Oktay Berk Altıntaş <65086193+oktayberk@users.noreply.github.com>
2022-12-05 21:47:14 +0300

    Update Main.java

--- throwDemo/src/Main.java
+++ throwDemo/src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        
         AccountManager manager=new AccountManager();
+        
         System.out.println("hesap="+manager.getBalance());
+        
         manager.deposit(100);//hesaba 100 lıra yatırdık
+        
         System.out.println("hesap="+manager.getBalance());
+        
         try {
             manager.withdraw(90);//90 lira cektik
         } catch (BalanceInsufficentException exception) {
